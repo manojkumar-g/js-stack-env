76286450c0204dffe4b1b50f640da40f8a4714cd
osgdev <osgdev@DHZ-MeanStk03.wipro.com>
2017-04-27 14:20:33 +0000

    configuring scripts

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-var path = require('path');
-var webpack = require('webpack');
-var express = require('express');
-//var config = require('./webpack.config');
-var config = require('./config/webpack/dev');
+var path = require('path')
 
-var app = express();
-var compiler = webpack(config);
+var express = require('express')
+//var config = require('./webpack.config')
 
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath,
-  hot : true,
-  noInfo : true,
-  stats: {
-      colors: true
-    }
-}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+const isDevelopment = process.argv.indexOf('--dev') !== -1
+var app = express()
+
+
+if(isDevelopment){
+  var webpack = require('webpack')
+  var config = require('./config/webpack/dev')
+  var compiler = webpack(config)
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath,
+    hot : true,
+    noInfo : true,
+    stats: {
+        colors: true
+      }
+  }))
+  app.use(require('webpack-hot-middleware')(compiler))
+}
+else{
+  app.use(express.static(__dirname + '/dist'));
+}
+
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, config.output.path+'/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 app.listen(1234, function(err) {
   if (err) {
-    return console.error(err);
+    return console.error(err)
   }
 
-  console.log('Listening at http://localhost:1234/');
+  console.log('Listening at http://localhost:1234/')
 })
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component{
   render(){
     return(
       <div className="container">
-        <h1>Que Pasa? manoj</h1>
+        <h1>Como Esthas?</h1>
       </div>
     );
   }
