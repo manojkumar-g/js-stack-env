ef1ea4ff05770e46e04378295437273b7cc6f929
manojkumar-g <manoj.g.kumar@hotmail.com>
2017-05-08 13:45:10 +0000

    finising client side render

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-app.listen(1234, function(err) {
+app.listen(3000, function(err) {
   if (err) {
     return console.error(err)
   }
--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from'./components/app.jsx'
+import App from'../shared/app.jsx'
 import { AppContainer } from 'react-hot-loader'
+import { BrowserRouter } from 'react-router-dom'
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
+    wrapApp(App),
     document.getElementById('root')
   )
 }
 
+const wrapApp = (AppComponent) =>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
+
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./components/app.jsx', () => {
+  module.hot.accept('../shared/app.jsx', () => {
     render(App)
   });
 }
